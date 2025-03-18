18e79a97bd449de006a14cfa09aff65b511f82ec
Mila-Amen <million.degefu@dci-student.org>
2025-03-18 11:51:30 +0100

    Initial commit

---
+++ models/imageSchema.js
@@ -0,0 +1,8 @@
+import { Schema, model } from "mongoose";
+
+const imageSchema = new Schema({
+  filename: { type: String, required: true },
+  data: { type: Buffer, required: true },
+});
+const ImageModel = model("Image", imageSchema);
+export default ImageModel;
---
+++ models/productSchema.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+
+const productSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    decription: { type: String },
+    sizes: { type: String, enum: ["XL", "L", "M", "S", "XS"] },
+    price: { type: Number, required: true },
+    rating: { type: String },
+    inventory: { type: Number, required: true },
+    image_url: { type: String },
+  },
+  { timestamps: true }
+);
+
+//create a model
+const ProductsModel = model("Product", productSchema);
+
+export default ProductsModel;
+
+/* 
+gender : {type:String, enum:["male","female"]}, 
+role: {type:String, enum:["user","admin","manager"]} */
